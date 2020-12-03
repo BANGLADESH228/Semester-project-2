9b3a78f513c45502f07e3e215e2282542ba66a45
Dias <dias_bolatov10@mail.com>
2020-12-03 10:16:11 +0500

    Added authorization

---
+++ Semester-project-2 Back-end/Semester-project-2/Pages/registration.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Semester_project_2.Pages
+{
+    public class registrationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
