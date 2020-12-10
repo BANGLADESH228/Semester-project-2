929f051de5181b89ba764f25a4c69228629dfa7f
voda2600@mail.ru <voda2600@mail.ru>
2020-12-10 21:11:32 +0300

    FromtRady50%

---
+++ Semester-project-2 Back-end/Semester-project-2/Pages/Pushkino.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Semester_project_2.Pages
+{
+    public class PushkinoModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
